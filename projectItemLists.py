afc8868378b15bb8f31131405ddfd645f98d9397
415000 <camendoza@DIRECTV.com>
2017-01-08 23:47:25 -0800

    adding comments to each function

--- projectItemLists.py
+++ projectItemLists.py
@@ -31,7 +31,11 @@ session = DBSession()
 
 
 class Env:
-
+    """
+    Env class: used to send parameters to templates in a unifor way
+    Args:
+        title (data type: str): used in templates for title
+    """
     def __init__(self, title=None):
         if 'email' in login_session:
             self.__currentUser = login_session
@@ -69,6 +73,11 @@ class Env:
 
 @app.route('/login')
 def showLogin():
+    """
+    showLogin: displays login page
+    Returns:
+        return templates having a state and environment object
+    """
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
@@ -77,6 +86,15 @@ def showLogin():
 
 
 def login_required(f):
+    """
+    login_required decorator: used to validate if a particular method requires
+    start a session
+    Args:
+        f (data type: function): function to be validated for login
+    Returns:
+        returns the function to decorate or redirects to login page if access
+        is not granted
+    """
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'email' in login_session:
@@ -88,15 +106,18 @@ def login_required(f):
 
 
 @login_required
-def check_creator(category_id=None, item_id=None):
-    # @wraps(f)
-    # def decorated_function(*args, **kwargs):
+def check_creator(category_id=None):
+    """
+    check_creator method: validates the creator to allow operations such as
+    edit and delete
+    Args:
+        category_id (data type: int): used to get user_id for category
+    Returns:
+        returns True or False to allow or deny UD operations
+    """
     if category_id:
         editCat = session.query(Category).filter_by(id=category_id).one()
         user = editCat.user_id
-    if item_id:
-        editedItem = session.query(Item).filter_by(id=item_id).one()
-        user = editedItem.user_id
     if user == login_session['email']:
         return True
     else:
@@ -108,6 +129,11 @@ def check_creator(category_id=None, item_id=None):
 # Store it in the session for latter validation.
 @app.route('/fbconnect', methods=['POST'])
 def fbconnect():
+    """
+    fbconnect: provides means to authenticate and authorize via fb apis
+    Returns:
+        returns the html with the results of the operation
+    """
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter'), 401)
         response.headers['Content-Type'] = 'application/json'
@@ -144,8 +170,6 @@ def fbconnect():
     user_id = getUserID(login_session['email'])
     if not user_id:
         user_id = createUser(login_session)
-    else:
-        updateUser(login_session)
     login_session['email'] = user_id
     output = ''
     output += 'Welcome, '
@@ -161,6 +185,11 @@ def fbconnect():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    """
+    gconnect: provides means to authenticate and authorize via google apis
+    Returns:
+        returns the html with the results of the operation
+    """
     # return request.args.get('state')
     # return login_session['state']
     if request.args.get('state') != login_session['state']:
@@ -229,8 +258,6 @@ def gconnect():
 
     if not user_id:
         user_id = createUser(login_session)
-    else:
-        updateUser(login_session)
     login_session['user_id'] = user_id
 
     output = ''
@@ -247,6 +274,11 @@ def gconnect():
 
 @app.route('/fbdisconnect')
 def fbdisconnect():
+    """
+    fbconnect: provides means to disconnect from fb
+    Returns:
+        returns message after disconnected
+    """
     # Only disconnect a connected user
     facebook_id = login_session.get('facebook_id')
     url = 'https://graph.facebook.com/%s/permissions' % facebook_id
@@ -258,6 +290,11 @@ def fbdisconnect():
 
 @app.route('/gdisconnect')
 def gdisconnect():
+    """
+    gdisconnect: provides means to disconnect from google
+    Returns:
+        returns json message after disconnected
+    """
     # Only disconnect a connected user
     credentials = login_session.get('credentials')
     if credentials is None:
@@ -280,6 +317,11 @@ def gdisconnect():
 
 @app.route('/disconnect')
 def disconnect():
+    """
+    disconnect: generic method used for both fb and google disconnect
+    Returns:
+        return redirection to categories page
+    """
     if 'provider' in login_session:
         if login_session['provider'] == 'google':
             gdisconnect()
@@ -302,13 +344,20 @@ def disconnect():
         return redirect(url_for('showCategories'))
 
 # PROJECT RELATED
-
 # JSON APIs to view Category Information
 
 
 @app.route('/category/<int:category_id>/item/JSON')
 @login_required
 def categoryItemJSON(category_id):
+    """
+    categoryItemJSON method: displays items related to a category in JSON
+    format
+    Args:
+        category_id (data type: int): category to be used to query items
+    Returns:
+        returns serialized JSON with a list of Items
+    """
     category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Item).filter_by(category_id=category_id).all()
     return jsonify(Items=[i.serialize for i in items])
@@ -317,6 +366,13 @@ def categoryItemJSON(category_id):
 @app.route('/item/<int:item_id>/JSON')
 @login_required
 def itemJSON(item_id):
+    """
+    itemJSON method: displays a single item based on ID in JSON format
+    Args:
+        item_id (data type: int): item to be used to query items
+    Returns:
+        returns serialized JSON with a list of Items
+    """
     item = session.query(Item).filter_by(id=item_id).one()
     return jsonify(item=item.serialize)
 
@@ -324,6 +380,11 @@ def itemJSON(item_id):
 @app.route('/category/JSON')
 @login_required
 def categoriesJSON():
+    """
+    categoriesJSON method: displays all categories in JSON format
+    Returns:
+        returns serialized JSON with a list of Categories
+    """
     categories = session.query(Category).order_by(Category.name).all()
     return jsonify(categories=[c.serialize for c in categories])
 
@@ -331,6 +392,11 @@ def categoriesJSON():
 @app.route('/user/JSON')
 @login_required
 def userJSON():
+    """
+    userJSON method: displays all users in JSON format
+    Returns:
+        returns serialized JSON with a list of Users
+    """
     users = session.query(User).order_by(User.name).all()
     return jsonify(users=[u.serialize for u in users])
 
@@ -338,6 +404,11 @@ def userJSON():
 @app.route('/allitems/JSON')
 @login_required
 def allItemsJSON():
+    """
+    allItemsJSON method: displays all items in JSON format
+    Returns:
+        returns serialized JSON with a list of Items
+    """
     items = session.query(Item).order_by(Item.created_date).all()
     return jsonify(items=[i.serialize for i in items])
 
@@ -346,6 +417,11 @@ def allItemsJSON():
 @app.route('/')
 @app.route('/category/')
 def showCategories():
+    """
+    showCategories method: generates categories page based on session status
+    Returns:
+        return categories page
+    """
     categories = session.query(Category).order_by(asc(Category.name)).all()
     items = session.query(Item).order_by(desc(Item.created_date)).all()
     env = Env()
@@ -364,6 +440,11 @@ def showCategories():
 @app.route('/category/new/', methods=['GET', 'POST'])
 @login_required
 def newCategory():
+    """
+    newCategory method: generates  new category page based on session status
+    Returns:
+        return new category page
+    """
     if 'username' not in login_session:
         return redirect('/login')
     if request.method == 'POST':
@@ -383,6 +464,13 @@ def newCategory():
 @app.route('/category/<int:category_id>/edit/', methods=['GET', 'POST'])
 @login_required
 def editCategory(category_id):
+    """
+    editCategory method: generates edit category page based on session status
+    Args:
+    category_id (data type: int): category to be edited
+    Returns:
+        return edit category page
+    """
     if check_creator(category_id):
         editedCategory = session.query(
             Category).filter_by(id=category_id).one()
@@ -403,6 +491,13 @@ def editCategory(category_id):
 @app.route('/category/<int:category_id>/delete/', methods=['GET', 'POST'])
 @login_required
 def deleteCategory(category_id):
+    """
+    deleteCategory method: generates  new category page based on session status
+    Args:
+    category_id (data type: int): category to be deleted
+    Returns:
+        return new category page
+    """
     if check_creator(category_id):
         categoryToDelete = session.query(
             Category).filter_by(id=category_id).one()
@@ -423,6 +518,14 @@ def deleteCategory(category_id):
 @app.route('/category/<int:category_id>/')
 @app.route('/category/<int:category_id>/item/')
 def showItem(category_id):
+    """
+    method/class name: short description
+    Args:
+        arg1 (data type: int/str/ etc): argument description
+        etc ...
+    Returns:
+        return value description
+    """
     category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Item).filter_by(
         category_id=category_id).order_by(asc(Item.created_date)).distinct()
@@ -444,6 +547,14 @@ def showItem(category_id):
 @app.route('/category/<int:category_id>/item/new/', methods=['GET', 'POST'])
 @login_required
 def newItem(category_id):
+    """
+    newItem method: generates new item page based on session status
+    Args:
+    category_id (data type: int): category associated to the item to be
+    created.
+    Returns:
+        return new item page depending on session status
+    """
     if check_creator(category_id):
         category = session.query(Category).filter_by(id=category_id).one()
         env = Env()
@@ -468,6 +579,14 @@ def newItem(category_id):
            methods=['GET', 'POST'])
 @login_required
 def editItem(category_id, item_id):
+    """
+    editItem method: generates edit item page based on session status
+    Args:
+    category_id (data type: int): category associated to the item to be
+    edited.
+    Returns:
+        return edit item page depending on session status
+    """
     if check_creator(category_id):
         editedItem = session.query(Item).filter_by(id=item_id).one()
         category = session.query(Category).filter_by(id=category_id).one()
@@ -492,6 +611,15 @@ def editItem(category_id, item_id):
            methods=['GET', 'POST'])
 @login_required
 def deleteItem(category_id, item_id):
+    """
+    deleteItem method: generates edit item page based on session status
+    Args:
+    category_id (data type: int): category associated to the item to be deleted
+    item_id (data type: int): item to be deleted
+    Returns:
+        return delete item page depending on session status and redirect to the
+        category page when item was deleted
+    """
     if check_creator(category_id):
         category = session.query(Category).filter_by(id=category_id).one()
         itemToDelete = session.query(Item).filter_by(id=item_id).one()
@@ -510,6 +638,13 @@ def deleteItem(category_id, item_id):
 
 # GENERAL STUFF
 def getUserID(email):
+    """
+    getUserID method: get user id based on email
+    Args:
+    email (data type: str): email to be used to retreive ID
+    Returns:
+        returns user id
+    """
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.email
@@ -518,6 +653,13 @@ def getUserID(email):
 
 
 def getUserInfo(user_id):
+    """
+    getUserInfo method: gets a user object based on user_id
+    Args:
+    user_id (data type: str): email to be used to retreive user
+    Returns:
+        returns user object
+    """
     try:
         user = session.query(User).filter_by(email=user_id).one()
     except:
@@ -526,6 +668,13 @@ def getUserInfo(user_id):
 
 
 def createUser(login_session):
+    """
+    createUser method: creates user based on login session
+    Args:
+    login_session (data type: object): login_session to be used to create user
+    Returns:
+        returns email of newly created user
+    """
     newUser = User(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
     session.add(newUser)
@@ -533,14 +682,6 @@ def createUser(login_session):
     user = session.query(User).filter_by(email=login_session['email']).one()
     return user.email
 
-
-def updateUser(login_session):
-    user = User(name=login_session['username'], email=login_session[
-                'email'], picture=login_session['picture'])
-    user.picture = login_session['picture']
-    # session.add(user)
-    session.commit()
-    return
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
